b83e6f15919cf7f694016ae27cda08aeab3cb23a
wnbuitragoc <wnbuitragoc@correo.udistrital.edu.co>
2020-05-29 16:25:28 -0500

    Problema de las vueltas parcialmente terminado

---
+++ src/problema/de/las/monedas/ProblemaDeLasMonedas.java
@@ -0,0 +1,13 @@
+
+package problema.de.las.monedas;
+
+
+public class ProblemaDeLasMonedas {
+
+   
+    public static void main(String[] args) {
+        Vista miVista=new Vista();
+        miVista.setVisible(true);
+    }
+    
+}
